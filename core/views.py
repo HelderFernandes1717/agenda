a39376e6b58ec3e862f644be4a3db6347fb15ebb
HelderFernandes1717 <Helder@HELDER.solsis.br>
2021-05-22 14:20:52 -0300

    Inclusao

--- core/views.py
+++ core/views.py
@@ -17,7 +17,6 @@ def logout_users(request):
     logout(request)
     return redirect('/')
 
-
 def subimit_login(request):
     if request.POST:
         username = request.POST.get('username')
@@ -40,6 +39,23 @@ def lista_eventos(request):
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
+
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                             data_evento=data_evento,
+                             descricao=descricao,
+                             usuario=usuario)
+        return redirect('/')
 
 
 
